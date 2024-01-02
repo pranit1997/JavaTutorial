7b3c3d168095c9985299dbe0e54e6cdb1499e355
Pranay Tiwari <pranay.tiwari@yum.com>
2024-01-02 22:02:44 +0530

    --commit

--- Loops&Functions.js
+++ Loops&Functions.js
@@ -6,5 +6,19 @@ for (let i=0;i<n;i++)
 {
     sum += (i+1)
 };
-console.log("Sum of first "+ n +" natural number is "+ sum );
+console.log("Sum of first "+ n +" natural number is: "+ sum );
 
+// For in Loop
+let obj = 
+{
+    Pranit : 95,
+    Parth  : 45,
+    Pranay : 98
+}
+for(let a in obj)
+{console.log("Marks of "+a+ " are "+obj[a])}
+
+// For of Loop
+
+for (let b of "Pranit")
+{console.log(b)}
